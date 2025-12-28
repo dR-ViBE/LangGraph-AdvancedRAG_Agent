c9515cc2df90fd44e9f53949714f09d3c9ef4a77
ganesh <ganeshprabakaran876@gmail.com>
2025-12-28 17:55:12 -0500

    retrievaldocuments_grader_pipeline

---
+++ graph/chains/retrieval_grader.py
@@ -0,0 +1,21 @@
+from langchain_core.prompts import ChatPromptTemplate
+from pydantic import BaseModel,Field
+
+from langchain_groq import ChatGroq
+
+llm=ChatGroq(model="llama-3.1-8b-instant")
+
+class GradeDocuments(BaseModel):
+    """Binary score relevance check on retrieved documents"""
+
+    binary_score:str=Field(description="Documents are relevent to question,'yes' or 'no")
+
+structured_llm_grader=llm.with_structured_output(GradeDocuments)
+
+system="""You are a grader assessing relevance of a retrieved document to a user question. \n 
+    If the document contains keyword(s) or semantic meaning related to the question, grade it as relevant. \n
+    Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the question."""
+
+grade_prompt=ChatPromptTemplate.from_template([("system",system),("human","Retrieved document:\n\n {document}\n\n User question:{question}",)])
+
+retrieval_grader=grade_prompt|structured_llm_grader
